3e2b810f6d74f83b3eb823c1587e9f90444479af
Yap Zi Yan <ziyanyapzyy@gmail.com>
2025-04-29 21:43:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from constants import *
 import streamlit as st
 from pdfminer.high_level import extract_text
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -5,35 +6,49 @@ from transformers import pipeline
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import re
+import os
 import contractions
-import nltk
 from nltk.corpus import stopwords
 from nltk import pos_tag
 from nltk.tokenize import word_tokenize
 from deep_translator import GoogleTranslator
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
-
-# Ensure NLTK resources are available
-def download_nltk_resources():
-    resources = {
-        'stopwords': 'corpora',
-        'punkt': 'tokenizers',
-        'averaged_perceptron_tagger': 'taggers',
-        'wordnet': 'corpora'
-    }
-    for resource, resource_type in resources.items():
-        try:
-            nltk.data.find(f'{resource_type}/{resource}')
-        except LookupError:
-            nltk.download(resource)
-
-download_nltk_resources()
+import time
 
 STOPWORDS = set(stopwords.words('english'))
 
 # Set up the Streamlit page configuration
-st.set_page_config(page_icon='📑', page_title="PDF QnA Bot")
+st.set_page_config(page_icon='📑', page_title="Resume QnA Bot")
+
+# Ensure the directory for model storage exists
+def ensure_model_directory(base_dir="saved_models/qa_models"):
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+
+# Retrieve the full path for a model
+def get_model_path(model_name, base_dir="saved_models/qa_models"):
+    return os.path.join(base_dir, model_name.replace("/", "_"))
+
+# Save both the model and its tokenizer to the disk
+def save_model(model, tokenizer, model_path):
+    model.save_pretrained(model_path)
+    tokenizer.save_pretrained(model_path)
+
+# Load or download the model depending on its availability
+def load_or_download_model(model_name, base_dir="saved_models/qa_models"):
+    model_path = get_model_path(model_name, base_dir)
+    if not os.path.exists(model_path):
+        print(f"Downloading and saving model: {model_name}")
+        model = pipeline("question-answering", model=model_name)
+        save_model(model.model, model.tokenizer, model_path)
+    else:
+        print(f"Loading model from disk: {model_name}")
+        model = pipeline("question-answering", model=model_path, tokenizer=model_path)
+    return model
+
+# Initialize once at start
+ensure_model_directory()
 
 # Initialize or update session state variables
 def initialize_session_state():
@@ -41,6 +56,12 @@ def initialize_session_state():
         st.session_state.uploaded = False
     if "messages" not in st.session_state:
         st.session_state.messages = []
+
+# Cache the loaded embedding models
+@st.cache_resource
+def load_embedding_models():
+    return {model_key: HuggingFaceEmbeddings(model_name=model_name, model_kwargs={"trust_remote_code": True})
+            for model_key, model_name in EMBEDDING_MODEL_NAMES.items()}
 
 # Toggle the state of the uploader button and clear messages
 def toggle_uploader():
@@ -50,23 +71,24 @@ def toggle_uploader():
 # Extract and clean text from PDF
 def extract_and_clean_text_from_pdf(uploaded_file):
     raw_text = extract_text(uploaded_file)
-    cleaned_text = re.sub(r'\n\s*\n', '\n', re.sub(r'\s+', ' ', raw_text.strip()))
-    return cleaned_text
+    return re.sub(r'\n\s*\n', '\n', re.sub(r'\s+', ' ', raw_text.strip()))
 
 # Remove non-ASCII characters from text
-def remove_non_ascii(text):
-    return text.encode(encoding="ascii", errors="ignore").decode('ascii')
+def removeNonASCII(text):
+    encoded_text = text.encode(encoding="ascii", errors="ignore")
+    return encoded_text.decode('ascii')
 
 # Chunk text using the specified text splitter
 def chunk_text(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = text_splitter.split_text(text=text)
-    return [remove_non_ascii(chunk) for chunk in chunks]
+    return [removeNonASCII(chunk) for chunk in chunks]
 
 # Generate a response from the QA model based on the context and prompt
-def generate_response(qa_model, prompt, context):
+def generate_response(selected_model, prompt, context):
     try:
-        return qa_model({'question': prompt, 'context': context})['answer']
+        qa_pipeline = selected_model
+        return qa_pipeline({'question': prompt, 'context': context})['answer']
     except Exception as e:
         st.error(f"Failed to generate response: {e}")
         return "An error occurred while generating the response."
@@ -80,14 +102,29 @@ def reset_embedding():
     if "db" in st.session_state:
         st.session_state.pop("db")
 
+def reset_qa():
+    clear_chat()
+    if "qa_model" in st.session_state:
+        st.session_state.pop("qa_model")
+
 # Processes the user query
 def preprocess_query(text):
     lemmatizer = WordNetLemmatizer()
 
-    text = re.sub(r'\s+', ' ', text.strip())  # Normalize spaces
-    text = GoogleTranslator(source='auto', target='en').translate(text)  # Translate to English
-    text = contractions.fix(text).lower()  # Expand contractions and convert to lowercase
-    text = re.sub(r'[^\w\s]', '', text)  # Remove non-alphanumeric characters
+    # Normalize spaces
+    text = re.sub(r'\s+', ' ', text.strip())
+
+    # Translate text to English
+    text = GoogleTranslator(source='auto', target='en').translate(text=text)
+    
+    # Expand contractions (e.g., "isn't" to "is not")
+    text = contractions.fix(text)
+    
+    # Convert text to lowercase to standardize it
+    text = text.lower()
+    
+    # Remove non-alphanumeric characters
+    text = re.sub(r'[^\w\s]', '', text)
 
     # Part-of-Speech tagging
     pos_tagged = pos_tag(word_tokenize(text))
@@ -115,23 +152,27 @@ def preprocess_query(text):
 # Main function to run the Streamlit app
 def main():
     initialize_session_state()
-    st.title("PDF Question-Answering App📃")
+    st.title("Resume Question-Answering App📃")
 
     with st.sidebar:
-        uploaded_file = st.file_uploader("Upload a PDF", type=["pdf"], disabled=st.session_state.uploaded, on_change=toggle_uploader)
+        uploaded_file = st.file_uploader("Upload a resume (PDF)", type=["pdf"], disabled=st.session_state.uploaded, on_change=toggle_uploader)
+        selected_embedding_model_name = st.radio("Select an Embedding Model:", list(EMBEDDING_MODEL_NAMES.keys()), on_change=reset_embedding)
+        selected_QA_model_name = st.radio("Select a QA Model:", list(QA_MODEL_NAMES.values()), on_change=reset_qa)
 
         # Define a button to start the chat which loads models, processes text, etc.
         if st.button("Start Chat"):
-            if uploaded_file:
+            if uploaded_file is not None:
                 if "db" not in st.session_state:
-                    with st.spinner('Processing PDF...'):
-                        # Extract, clean, and chunk text from PDF
-                        PDF_text = extract_and_clean_text_from_pdf(uploaded_file)
-                        chunks = chunk_text(PDF_text)
+                    with st.spinner('Processing resume...'):
+                        # Extract text from PDF
+                        resume_text = extract_and_clean_text_from_pdf(uploaded_file)
+                        
+                        # Chunk text
+                        chunks = chunk_text(resume_text)
 
                     with st.spinner('Loading embedding model...'):
                         # Load embedding model
-                        embedding_model = HuggingFaceEmbeddings(model_name="Alibaba-NLP/gte-base-en-v1.5", model_kwargs={"trust_remote_code": True})
+                        embedding_model = load_embedding_models().get(selected_embedding_model_name)
 
                     with st.spinner('Storing embeddings...'):
                         # Store embeddings
@@ -141,13 +182,14 @@ def main():
                 if "qa_model" not in st.session_state:
                     with st.spinner('Loading QA model...'):
                         # Load QA model
-                        QA_model = pipeline("question-answering", model="deepset/bert-large-uncased-whole-word-masking-squad2")
+                        QA_model_key = [key for key, value in QA_MODEL_NAMES.items() if value == selected_QA_model_name][0]
+                        QA_model = load_or_download_model(QA_model_key)
                         st.session_state.qa_model = QA_model  # Store the QA model in session state
                 
-                st.success('Ready to answer questions from the PDF!')
+                st.success('Ready to answer questions from the resume!')
             
             else:
-                st.error('Please upload a PDF to start the chat.')
+                st.error('Please upload a resume PDF to start the chat.')
 
     # Display chat history
     if st.session_state.messages:
@@ -157,7 +199,10 @@ def main():
 
     if 'db' in st.session_state and 'qa_model' in st.session_state:
         # Accept user query input
-        if prompt := st.chat_input("Ask a question about the PDF..."):
+        if prompt := st.chat_input("Ask a question about the resume..."):
+            # Track response time from submitting query until response is diplayed
+            tic = time.time()
+
             # Preprocessing on user's query
             with st.spinner("Processing your question..."):
                 cleaned_prompt = preprocess_query(prompt)
@@ -183,6 +228,8 @@ def main():
             with st.chat_message("assistant"):
                 st.markdown(response)
 
+            toc = time.time()
+            print(f"Response generated in {toc - tic:0.4f} seconds")
 
 if __name__ == "__main__":
     main()
